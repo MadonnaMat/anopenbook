4d5083fc669f9283b05a1a677fe5b62fba9df1a4
Matthew Madonna <madonkadonk@gmail.com>
2018-04-21 17:28:10 -0400

    pug and scss

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,22 +1,24 @@
-<template>
-  <div id="app">
-    <p>{{ message }}</p>
-  </div>
+<template lang="pug">
+  #test
+    h1 Welcome
+    p {{ message }}
 </template>
 
 <script>
 export default {
-  data: function () {
+  data: function() {
     return {
-      message: "Hello Vue!"
-    }
-  }
-}
+      message: 'Hello Vue!',
+    };
+  },
+};
 </script>
 
-<style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
+<style lang="scss" scoped>
+#test {
+  p {
+    font-size: 2em;
+    text-align: center;
+  }
 }
 </style>
