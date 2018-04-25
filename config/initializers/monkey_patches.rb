9fd9c0c58314c0957499c963a22739912dcac0aa
Matthew Madonna <madonkadonk@gmail.com>
2018-04-24 22:18:53 -0400

    Monkey Patch File Open

---
+++ config/initializers/monkey_patches.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class File
+  def self.open(*args)
+    if args[0].is_a?(StringIO)
+      begin
+        string = args[0]
+        tmp = Tempfile.new
+        tmp.binmode
+        tmp.write string.read
+        tmp.rewind
+        args[0] = tmp
+        string.rewind
+      rescue StandardError
+      end
+    end
+
+    file = super(*args)
+    file
+  end
+end
