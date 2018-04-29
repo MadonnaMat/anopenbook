a8a881e860535b858be542d12cd983bedd65dfcc
Matthew Madonna <madonkadonk@gmail.com>
2018-04-29 11:33:21 -0400

    Switch app name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,13 +72,13 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # TODO: update for production
-  config.action_mailer.default_url_options = { host: 'whispering-coast-25887.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'anopenbook.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
-    domain: 'whispering-coast-25887.herokuapp.com',
+    domain: 'anopenbook.herokuapp.com',
     address: 'smtp.sendgrid.net',
     port: 587,
     authentication: :plain,
